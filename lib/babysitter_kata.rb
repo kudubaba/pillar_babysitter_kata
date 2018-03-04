30504db16bb3192fa95d6846c2eb144c068424d9
kudubaba <kudubaba@gmail.com>
2018-03-03 23:05:39 -0500

    Finished test for value of num3 variable

--- lib/babysitter_kata.rb
+++ lib/babysitter_kata.rb
@@ -1,19 +1,8 @@
 class Babysitter
+  attr_accessor :num1, :num2, :num3
   def initialize(num1, num2, num3)
-    @num1 = num1
-    @num2 = num2
-    @num3 = num3
-  end
-
-  def num1
-    @num1
-  end
-
-  def num2
-    @num2
-  end
-
-  def num3
-    @num3
+    # @num1 = num1
+    # @num2 = num2
+    # @num3 = num3
   end
 end
