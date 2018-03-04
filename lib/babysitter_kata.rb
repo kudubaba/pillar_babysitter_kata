8012686d579fecb95fad25e3b0ab8e5a70031761
kudubaba <kudubaba@gmail.com>
2018-03-03 23:09:12 -0500

    Refactored Babysitter class code

--- lib/babysitter_kata.rb
+++ lib/babysitter_kata.rb
@@ -1,8 +1,8 @@
 class Babysitter
   attr_accessor :num1, :num2, :num3
   def initialize(num1, num2, num3)
-    # @num1 = num1
-    # @num2 = num2
-    # @num3 = num3
+    @num1 = num1
+    @num2 = num2
+    @num3 = num3
   end
 end
