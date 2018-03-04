b916208a73ba6f1a07547ed3a2fee72e538b643c
kudubaba <kudubaba@gmail.com>
2018-03-03 23:12:42 -0500

    Refactored Babysitter Class Tests

--- spec/babysitter_kata_spec.rb
+++ spec/babysitter_kata_spec.rb
@@ -1,16 +1,17 @@
 require '../lib/babysitter_kata'
 
 RSpec.describe Babysitter do
+  let (:schedule1){Babysitter.new(18,22,3)}
   it '.new creates a new instance of babysitter' do
-    expect(Babysitter.new(18,22,3)).to be_an_instance_of Babysitter
+    expect(schedule1).to be_an_instance_of Babysitter
   end
   it '#num1 should return the value of num1' do
-    expect(Babysitter.new(18,22,3).num1).to eql(18)
+    expect(schedule1.num1).to eql(18)
   end
   it '#num2 should return the value of num2' do
-    expect(Babysitter.new(18,22,3).num2).to eql(22)
+    expect(schedule1.num2).to eql(22)
   end
   it '#num3 should return the value of num3' do
-    expect(Babysitter.new(18,22,3).num3).to eql(3)
+    expect(schedule1.num3).to eql(3)
   end
 end
