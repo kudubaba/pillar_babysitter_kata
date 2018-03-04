addf2e4af017124220038d210103ec8e4e1ece86
kudubaba <kudubaba@gmail.com>
2018-03-03 22:53:51 -0500

    Finished test for value of num1 variable

--- lib/babysitter_kata.rb
+++ lib/babysitter_kata.rb
@@ -1,5 +1,9 @@
 class Babysitter
   def initialize(num1, num2, num3)
+    @num1 = num1
+  end
 
+  def num1
+    @num1
   end
 end
--- spec/babysitter_kata_spec.rb
+++ spec/babysitter_kata_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe Babysitter do
   it '.new creates a new instance of babysitter' do
     expect(Babysitter.new(18,22,3)).to be_an_instance_of Babysitter
   end
+  it '#num1 should return the value of num1' do
+    expect(Babysitter.new(18,22,3).num1).to eql(18)
+  end
 end
